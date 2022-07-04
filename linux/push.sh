545aea726e5c23fbc7a64e01da08c7a8cc80de32
denislav gavrilov <denislav.gavrilov@skf.com>
2022-07-04 19:04:58 +0300

    automated push

---
+++ linux/push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+command1=$1
+
+if [[ $command1 == "" ]]; then
+	echo "Executing autopush."
+	echo "If you want to stop this, better do it quick. You have 3 seconds."
+	sleep 1
+	echo "1..."
+	sleep 1
+	echo "2..."
+	sleep 1
+	echo "3..."
+	sleep 1
+	
+	branch=$(git status | grep "On branch" | sed 's/On branch//')
+	git add . && git commit -m "automated push" && git push origin $branch
+fi
